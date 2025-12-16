48cabd9b2a13a6162e7beee2365e2056c789c79f
Fajar Maulana <fajarma21@gmail.com>
2025-12-16 15:55:03 +0700

    feat(route): 404 route style

--- src/App/components/Main/components/Finished/View.tsx
+++ src/App/components/Main/components/Finished/View.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { FaRegFileLines } from 'react-icons/fa6';
 
 import Card from '@/components/Card';
--- src/App/components/NotFound/View.tsx
+++ src/App/components/NotFound/View.tsx
@@ -1,10 +1,23 @@
-// import css from './View.module.scss';
+import { useNavigate } from 'react-router';
+import { Button } from 'antd';
+
+import css from './View.module.scss';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate('/', { replace: true });
+  };
+
   return (
-    <>
-      <h2>404</h2>
-    </>
+    <div className={css.container}>
+      <h2 className={css.notFound}>404</h2>
+      <p className={css.desc}>you will get nothing here</p>
+      <Button color="primary" variant="outlined" onClick={handleRedirect}>
+        Let's go home
+      </Button>
+    </div>
   );
 };
 
