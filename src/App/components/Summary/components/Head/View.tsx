14eaecb7b52399ecd43f40005e342fec9f220db6
Fajar Maulana <47219560+fajarma21@users.noreply.github.com>
2025-12-16 15:55:49 +0700

    feat(route): 404 route style (#10)

--- src/App/components/Summary/components/Head/View.tsx
+++ src/App/components/Summary/components/Head/View.tsx
@@ -1,8 +1,9 @@
-import Button from '@/components/Button';
+import { useNavigate } from 'react-router';
 import { FaArrowLeft } from 'react-icons/fa6';
 
+import Button from '@/components/Button';
+
 import css from './View.module.scss';
-import { useNavigate } from 'react-router-dom';
 
 const Head = () => {
   const navigate = useNavigate();
